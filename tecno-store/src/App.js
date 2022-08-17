dcb9cbb08704f4284c8bd1820f558ae5b63634e1
Lao <estanislaolanus@gmail.com>
2022-08-16 21:21:41 -0300

    first commit

---
+++ tecno-store/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{background:"red", borderRadius: "3px", padding: "5px"}}>Hola mundo con react</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
