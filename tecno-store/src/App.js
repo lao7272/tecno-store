20ec002cb78c1e5579915f09660cd6fa9ea14b40
Lao <estanislaolanus@gmail.com>
2022-08-18 22:31:36 -0300

    Hice el navbar

--- tecno-store/src/App.js
+++ tecno-store/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
+import {Navbar} from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 style={{background:"red", borderRadius: "3px", padding: "5px"}}>Hola mundo con react</h1>
-      </header>
+      <Navbar/>
+      
     </div>
   );
 }
---
+++ tecno-store/src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+export function Navbar (){
+    return(
+        <>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Tecno Store</a>
+                    <div class="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Contacto</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
